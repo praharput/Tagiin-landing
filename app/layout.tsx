34b45efbe929eafba75ec6c0a125cb252920d3c5
praharput <praharput@gmail.com>
2026-05-10 18:40:43 +0700

    fix mobile viewport and pricing grid

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Tagiin',
     description: 'Tagih lebih mudah, dibayar lebih cepat.',
-    url: 'https://tagiin.app',
+    url: 'https://tagiin.id',
     siteName: 'Tagiin',
     locale: 'id_ID',
     type: 'website',
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="id">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
